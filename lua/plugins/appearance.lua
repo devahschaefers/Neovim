4b4e34feea46d15132dad5e3e46935305faddaf4
Devah Schaefers <73009338+devahschaefers@users.noreply.github.com>
2025-07-24 03:07:17 -0700

    fix indentiation

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -1,11 +1,12 @@
 -- should prolly move this somewhere else
-            function setColors(color)
-                color = color or "dracula-soft"
-                vim.cmd.colorscheme(color)
+function setColors(color)
+    color = color or "dracula-soft"
+    vim.cmd.colorscheme(color)
 
-                vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-                vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
-            end
+    vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+    vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+end
+
 return {
     {
         'nvim-lualine/lualine.nvim',
